7fa00a7b42f6c8e8b0acc65f685ab334e55acb26
jaag5678 <jaag5678>
2016-06-06 19:41:38 +0530

    ImpChanges

---
+++ Importante.h
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+#define _CRT_SECURE_NO_WARNINGS
+
+using namespace std;
---
+++ Main.cpp
@@ -0,0 +1,28 @@
+#include"Importante.h"
+#include"DFA.h"
+#include"NFA.h"
+
+int main() {
+
+	int S, V;
+	char *STR = NULL;
+
+	cout << "No of states " << endl;
+	cin >> S;
+
+	cout << "No of variables" << endl;
+	cin >> V;
+
+	NFA a(S, V);
+
+	a.Str_Input_N();
+
+	char c[14] = "0001100011000";
+
+	bool t = a.Str_Test_N(c);
+	cout <<t<< endl;
+
+	getchar(); //Optional 
+	getchar(); //Optional
+	return 0;
+}
